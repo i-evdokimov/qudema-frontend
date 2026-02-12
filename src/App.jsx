3f6f84840ab39e4029f316925e156af2dc77f447
i.evdokimov <i.evdokimov@g.nsu.ru>
2026-02-13 00:02:59 +0700

    feat: add Courses and About pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider, useAuth } from './context/AuthContext';
 
-// 1. ИМПОРТИРУЕМ КОМПОНЕНТЫ (Шапку и Подвал)
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-// Импорт страниц
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+// ИМПОРТИРУЕМ НОВЫЕ СТРАНИЦЫ
+import Courses from './pages/Courses';
+import About from './pages/About';
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -23,20 +24,20 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        {/* Обертка для правильного позиционирования футера */}
         <div className="flex flex-col min-h-screen font-sans text-dark">
-          
-          {/* 2. ВСТАВЛЯЕМ НАВБАР */}
           <Navbar />
-          
           <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
           
-          {/* 3. ОСНОВНОЙ КОНТЕНТ (pt-20 отступ сверху, чтобы Навбар не закрывал контент) */}
           <main className="flex-grow pt-20">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              
+              {/* ДОБАВЛЯЕМ МАРШРУТЫ */}
+              <Route path="/courses" element={<Courses />} />
+              <Route path="/about" element={<About />} />
+
               <Route 
                 path="/dashboard" 
                 element={
@@ -49,9 +50,7 @@ function App() {
             </Routes>
           </main>
 
-          {/* 4. ВСТАВЛЯЕМ ФУТЕР */}
           <Footer />
-          
         </div>
       </AuthProvider>
     </Router>
